4072fe176ef770d543bf7e8b9e43e23ef97379bc
sidmohanty11 <sidmohanty11@gmail.com>
2021-12-04 08:21:54 +0530

    basic auth:done

--- stories/pages/index.js
+++ stories/pages/index.js
@@ -1,23 +1,32 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Button, Card } from "semantic-ui-react";
 import StoryContainer from "../components/Container";
 import StoryHeader from "../components/Header";
 import Link from "next/link";
+import { auth } from "../services/auth";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    if (auth.currentUser) {
+      setCurrentUser(auth.currentUser);
+    }
+  }, []);
   return (
     <div>
       <Head>
         <title>stories</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <StoryHeader />
-      <Card.Group centered>
-        <Link href="/post-story" passHref>
-          <Button color="black">Post your Story</Button>
-        </Link>
-      </Card.Group>
+      <StoryHeader currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      {currentUser && (
+        <Card.Group centered>
+          <Link href="/post-story" passHref>
+            <Button color="black">Post your Story</Button>
+          </Link>
+        </Card.Group>
+      )}
       <StoryContainer />
     </div>
   );
